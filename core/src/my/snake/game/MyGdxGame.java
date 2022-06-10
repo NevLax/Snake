a7a8967e71c5be3fc88df899950229bbdfde411b
NevLax <qwegornikitin@gmail.com>
2022-06-10 14:35:33 +0400

    Initial commit

---
+++ core/src/my/snake/game/MyGdxGame.java
@@ -0,0 +1,18 @@
+package my.snake.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class MyGdxGame extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
